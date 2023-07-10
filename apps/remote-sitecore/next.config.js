3856eda6b55b8d3e6fa49a3e41bca10e7f21afe0
gskaushal92 <gskaushal92@gmail.com>
2023-07-10 16:13:08 +0530

    updated remote app configs

--- apps/remote-sitecore/next.config.js
+++ apps/remote-sitecore/next.config.js
@@ -81,24 +81,6 @@ const nextConfig = {
     ];
   },
 
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/brand',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
 
   // webpack: (config, options) => {
   //   const { isServer } = options;
